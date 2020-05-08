125dd04b71c9ada96dc4802fe603c53ce9bd8cc6
Alan Arturo Pedraza Luna <alanbanano@gmail.com>
2020-05-08 03:38:34 -0500

    Modulo de Inventario Completado (Editar y Eliminar), En Modulo Ventas se Centra el Boton

--- Estetica_Flask.py
+++ Estetica_Flask.py
@@ -4,7 +4,7 @@ from flask import render_template, request, redirect, url_for
 import pymysql.cursors
 
 # Connect to the database
-connection = pymysql.connect(host='localhost',
+con = pymysql.connect(host='localhost',
                              user='root',
                              password='Urb@no1125',
                              db='Estetica',
@@ -19,29 +19,29 @@ app = Flask(__name__, template_folder="templates")
 def index():
     return render_template('index.html')
     
-@app.route('/Inventario', methods=['GET','POST'])
+@app.route('/Inventario', methods=['GET'])
 def inventario():
+    cur = con.cursor()
+    cur.execute("SELECT * FROM Inventario2")
+    Inventario = cur.fetchall()
+    cur.close()
+    return render_template('inventario.html', Inventario = Inventario)
 
-    if request.method == "GET":
-       return render_template('inventario_form.html')
+@app.route('/inventario_form', methods=['GET'])
+def inventario_form():
+    return render_template('inventario_form.html')
 
-    else:
-        Marca_Producto = request.form['Marca_Producto']
-        Modelo_Producto = request.form['Modelo_Producto']
-        Costo_Producto = request.form['Costo_Producto']
-        Existencias = request.form['Existencias']
-
-        try:
-            with connection.cursor() as cursor:
-                # Agregar un nuevo Usuario
-                cursor.execute("INSERT INTO Inventario2 (Marca_Producto, Modelo_Producto, Costo_Producto, Existencias) VALUES (%s,%s,%s,%s)",(Marca_Producto, Modelo_Producto, Costo_Producto, Existencias))
-
-            # Conexion a la BD para Registro
-            connection.commit()
-        finally:
-            print("Se Guardo")
-
-        return render_template("index.html")     
+@app.route('/nuevo_producto', methods=['POST'])
+def nuevo_producto():
+    Marca_Producto = request.form['Marca_Producto']
+    Modelo_Producto = request.form['Modelo_Producto']
+    Costo_Producto = request.form['Costo_Producto']
+    Existencias = request.form['Existencias']
+    cur = con.cursor()
+    cur.execute("INSERT INTO Inventario2 (Marca_Producto, Modelo_Producto, Costo_Producto, Existencias) VALUES (%s,%s,%s,%s)",(Marca_Producto, Modelo_Producto, Costo_Producto, Existencias))
+    con.commit()
+    cur.close()
+    return redirect(url_for('inventario'))   
 
 @app.route('/Ventas',)
 def ventas():
@@ -75,6 +75,39 @@ def clientes():
 
         return render_template("index.html")        
 
+@app.route('/editar_producto/<id>', methods = ['POST', 'GET'])
+def get_contact(id):
+    cur = con.cursor()
+    cur.execute('SELECT * FROM Inventario2 WHERE ID_Producto = %s', (id))
+    data = cur.fetchall()
+    cur.close()
+    return render_template('editar_producto.html', producto = data[0])
+
+@app.route('/actualizar_producto/<id>', methods=['POST'])
+def update_contact(id):
+    Marca_Producto = request.form['Marca_Producto']
+    Modelo_Producto = request.form['Modelo_Producto']
+    Costo_Producto = request.form['Costo_Producto']
+    Existencias = request.form['Existencias']
+    cur = con.cursor()
+    cur.execute("""
+                UPDATE Inventario2 
+                SET Marca_Producto = %s ,
+                    Modelo_Producto = %s ,
+                    Costo_Producto = %s ,
+                    Existencias = %s
+                WHERE ID_Producto = %s
+                    """,(Marca_Producto, Modelo_Producto, Costo_Producto, Existencias, id))
+    con.commit()
+    cur.close()
+    return redirect(url_for('inventario'))
+
+@app.route('/eliminar_producto/<id>', methods = ['POST','GET'])
+def delete_contact(id):
+    cur = con.cursor()
+    cur.execute('DELETE FROM Inventario2 WHERE ID_Producto = %s',(id))
+    con.commit()
+    return redirect(url_for('inventario'))
 
 if __name__ == '__main__':
     app.run(debug = True, port=8000)
